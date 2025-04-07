b2bb32c9eb21fa9372e91587d354713cf8f74963
Isaaq Simon <IsaaqS>
2025-04-07 15:02:12 +0000

    Trapezoid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaaq Simon All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaaq Simon
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates area of a triangle.
+ */
+function calculate () {
+  // input
+  let base = parseInt(document.getElementById('base-of-triangle').value);
+  let height = parseInt(document.getElementById('height-of-triangle').value);
+
+  // process
+  let area = base * height / 2;
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
